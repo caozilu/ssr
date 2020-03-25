48b8a0abd7f9521bf80961139cde22219dc005e9
caozilu <caozilu@outlook.com>
2020-03-25 22:09:11 +0800

    add project base

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import './index.css';
+import Card from './Card';
+
 export default () => (
-    <p>Hello</p>
+    <div className="App">
+        <header className="App-header">
+            <img src="/static/logo.png" className="static-logo" alt="logo" />
+        </header>
+        <div className="Grid">
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    </div>
 );
